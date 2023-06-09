5e44ee8d360df5bf22a4b84b7cca6262ffddcf6b
dorad0 <dorado017@gmail.com>
2023-06-09 12:04:44 +0300

    add @PathVariable example (ex_4)

--- src/main/java/com/example/ch7_ex1/MainController.java
+++ src/main/java/com/example/ch7_ex1/MainController.java
@@ -2,15 +2,24 @@ package com.example.ch7_ex1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/home")
-    public String home(Model page) {
-        page.addAttribute("username", "Katy");
-        page.addAttribute("color", "red");
+    public String home(Model page, @RequestParam(required = false) String color, @RequestParam(required = false) String name) {
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
+        return "home.html";
+    }
+
+    @RequestMapping("/ex4/{color}")
+    public  String ex4(Model page, @PathVariable String color) {
+        page.addAttribute("username", "Lorry");
+        page.addAttribute("color", color);
         return "home.html";
     }
 }
