7a33e758f9f17e8f5534ed6eb04e8ec9c4922ee3
dorad0 <dorado017@gmail.com>
2023-06-09 11:45:48 +0300

    Initial commit

---
+++ src/main/java/com/example/ch7_ex1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.ch7_ex1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Ch7Ex1Application.class);
+	}
+
+}
